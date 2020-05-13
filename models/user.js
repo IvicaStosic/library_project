8388b596a9069c125b22b4908f9a1371ef5878e7
Ivica Stosic <ivica89stosic@gmail.com>
2020-05-14 00:41:36 +0200

    new commit

--- models/user.js
+++ models/user.js
@@ -21,6 +21,14 @@ const userSchema = new Schema({
     email: {
         type: String,
         required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
     }
 })
 
